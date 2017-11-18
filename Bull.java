9657be1ed9e8d40540b3297a830be7ca059c7dd7
scarystory <scarystoryify@gmail.com>
2017-11-17 20:50:03 -0700

    general

--- Bull.java
+++ Bull.java
@@ -2,13 +2,18 @@
 public class Bull extends CSprite
 {
     int m_damage = 10;
-    String m_type = "classic";
+    String m_type = "follow";
     int m_health = 30;
     int m_counter = 0;
+    boolean m_XMoving, m_YMoving;
+    int goalX,goalY;
+    int xSteps, ySteps;
+    
     public Bull(int x, int y, int z, Vector v, int speed)
     {
         super(x,y,z,v,new char[][]{{' ', '_', '(', '_', '_', ')', '_'}, {'\'', '-','0',' ',' ','0','-','\''},{' ','(','o','_','_','o',')',' '}}, speed);
     }
+    
 
     public Bull(int x, int y, int z, Vector v, int speed, int health)
     {
@@ -86,10 +91,61 @@ public class Bull extends CSprite
         CAvatar a = Canvas.get_Avatar();
         if (a != null)
         {
-            if (a.get_x() < m_x && isClear(m_x-1,m_y,m_z))m_x--;
-            if (a.get_x() > m_x && isClear(m_x+1,m_y,m_z))m_x++;
-            if (a.get_y() < m_y && isClear(m_x,m_y-1,m_z))m_y--;
-            if (a.get_y() > m_y && isClear(m_x,m_y+1,m_z))m_y++;
+            if (m_type.equals("classic")) 
+            {
+                if (xSteps<10) 
+                {
+                    goalX = a.get_x();
+                    xSteps++;
+                    ySteps=10;
+                } else if(ySteps==10){
+                    xSteps = 0;
+                    goalX = a.get_x();
+                    m_XMoving = true;
+                    m_YMoving = false;
+                }
+                if (ySteps<10) 
+                {
+                    goalY = a.get_y();
+                    ySteps++;
+                    xSteps=10;
+                } else if(xSteps==10){
+                    ySteps = 0;
+                    goalY = a.get_y();
+                    m_XMoving = false;
+                    m_YMoving = true;
+                }
+                if (!m_XMoving&&!m_YMoving) m_XMoving = true;
+                if(goalX == m_x)
+                {
+                    ySteps = 0;
+                    goalY = a.get_y();
+                    m_XMoving = false;
+                    m_YMoving = true;
+                } else if (goalY == m_y) {
+                    xSteps = 0;
+                    goalX = a.get_x();
+                    m_XMoving = true;
+                    m_YMoving = false;
+                }
+                if (m_XMoving)
+                {
+                    if (a.get_x() < m_x && isClear(m_x-1,m_y,m_z))m_x--;
+                    if (a.get_x() > m_x && isClear(m_x+1,m_y,m_z))m_x++;
+                }
+                if (m_YMoving) 
+                {
+                    if (a.get_y() < m_y && isClear(m_x,m_y-1,m_z))m_y--;
+                    if (a.get_y() > m_y && isClear(m_x,m_y+1,m_z))m_y++;
+                }
+            } 
+            else if(m_type.equals("follow"))
+            {
+                if (a.get_x() < m_x && isClear(m_x-1,m_y,m_z))m_x--;
+                if (a.get_x() > m_x && isClear(m_x+1,m_y,m_z))m_x++;
+                if (a.get_y() < m_y && isClear(m_x,m_y-1,m_z))m_y--;
+                if (a.get_y() > m_y && isClear(m_x,m_y+1,m_z))m_y++;
+            }
         }
         if (m_counter != 0) 
         {
--- VideoGame.java
+++ VideoGame.java
@@ -4,10 +4,10 @@ public class VideoGame
     {
         //CSprite s1 = new CSprite(6, 4, 0, new Vector(1, 0), new char[][]{{'1','2','3'}, {'4','5','6'}},2); 
         PowerUp p1 = new PowerUp(12, 12, 1, new Vector(1, 1), "randomkill",10);
-        Bull bull1 = new Bull(20, 10, 1, new Vector(1, 0),10);
+        Bull bull1 = new Bull(20, 10, 1, new Vector(1, 0),10,"classic");
         Scoreboard s1 = new Scoreboard("Score:", 1,38,0);
 //        CAvatar a1 = new CAvatar(8, 4, 0, new Vector(0, 0), new char[][]{{'a',' ','c'}, {'d','e','f'}});
-        Duck d1 = new Duck("avatartiny.txt", 18,20,0);
+        Duck d1 = new Duck("avatar.txt", 18,20,0);
         Background b1 = new Background("background.txt");
         //CSprite p1
         //b1.set_vector(new Vector(0,0));
